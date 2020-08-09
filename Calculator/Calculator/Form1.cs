2715df1c29049105be6c5e0e19624ec9983e17fe
KyleG <ssjkyle32@gmail.com>
2020-08-08 21:11:15 -0700

    Barebones layout

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -16,5 +16,15 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
